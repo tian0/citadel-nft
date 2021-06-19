71fd7cc6856a6f2123e310a73c9eaf272cbebe91
tian0 <25581521+tian0@users.noreply.github.com>
2021-06-19 13:28:40 -0400

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,6 @@ const web3 = require('../node_modules/web3');
 
 module.exports = function(deployer) {
   const reserveCurrency = '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b'; //Rinkeby USDC contract
-  const contribution = 1000000; //Rinkeby USDC has 6 decimals, this is 10 USDC for testing purposes
+  const contribution = 1000000; //Rinkeby USDC has 6 decimals, this is 1 USDC for testing purposes
   deployer.deploy(Citadel, reserveCurrency, contribution);
 };
